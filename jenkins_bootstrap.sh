09e598b56b0f96942a6106fe1b8af20b3f594604
Sam Gleske <sam.mxracer@gmail.com>
2018-04-21 10:18:40 -0700

    Update some settings

--- jenkins_bootstrap.sh
+++ jenkins_bootstrap.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 source jenkins-bootstrap-shared/jenkins_bootstrap.sh
+
+if [ ! -r settings.groovy -a -n "${GITHUB_TOKEN}" ]; then
+  sed -r "s#(String GITHUB_TOKEN = \")[^\"]+(\".*)#\\1${GITHUB_TOKEN}\\2#" settings.groovy.EXAMPLE > settings.groovy
+fi
+
+if [ -r settings.groovy ]; then
+  jenkins_console -s ./settings.groovy -s "${SCRIPT_LIBRARY_PATH}"/credentials-multitype.groovy
+fi
